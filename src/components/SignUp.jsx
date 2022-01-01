2ab88d44973ebc0ab38ee6f4bb19bc28393483d2
AltaraceO <orialtarace@gmail.com>
2022-01-01 20:44:55 +0200

    user update login/logout

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,20 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { UserContext } from "./UserContext";
 
 export const SignUp = () => {
-  const [user, setUser] = useContext(UserContext);
+  const [user] = useContext(UserContext)["user"];
+  const [current, setCurrent] = useContext(UserContext)["currUser"];
+  const [nameSearch, setNameSearch] = useState("");
 
   const onChangeHandle = (e) => {
-    console.log(e.target.value);
-    setUser(e.target.value);
+    setNameSearch(e.target.value);
+  };
+
+  const checkUser = () => {
+    const foundUser = user.find((e) => {
+      return e.user === nameSearch;
+    });
+    if (foundUser) {
+      setCurrent(foundUser);
+    } else {
+      setNameSearch("No user by that name");
+    }
   };
 
   return (
     <div>
-      <input type="text" value={user} onChange={onChangeHandle} />
-      <h2>{user}</h2>
+      <div className="ui input">
+        <input type="text" value={nameSearch} onChange={onChangeHandle} />
+      </div>
+      <h2>{nameSearch}</h2>
       <div>sign up or login</div>
+      <button
+        className={`ui  ${!nameSearch && "disabled"} basic button`}
+        onClick={checkUser}
+      >
+        <i className="icon user"></i>
+        {current ? "Logout" : "Login"}
+      </button>
     </div>
   );
 };
--- src/components/UserContext.jsx
+++ src/components/UserContext.jsx
@@ -5,23 +5,25 @@ export const UserContext = createContext();
 
 export const UserProvider = (props) => {
   const [user, setUser] = useState("");
+  const [current, setCurrent] = useState("");
 
   useEffect(() => {
     const getInfo = async () => {
       const { data } = await axios.get(
         "https://61d0790ccd2ee50017cc9887.mockapi.io/fbi/users"
       );
-      console.log(data);
-      //   setUser(userData);
+      setUser(data);
     };
 
-    if (true) {
+    if (!user) {
       getInfo();
     }
   }, [user]);
-
+  console.log(current);
   return (
-    <UserContext.Provider value={[user, setUser]}>
+    <UserContext.Provider
+      value={{ user: [user, setUser], currUser: [current, setCurrent] }}
+    >
       {props.children}
     </UserContext.Provider>
   );
